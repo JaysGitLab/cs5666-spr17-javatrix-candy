1e39f82a87d5dc60b6c084e99e95b68edb5e618c
Matt Stone <mattstone22133@gmail.com>
2017-02-07 13:07:25 -0500

    set up some checkstyle stuff - there is a problem with checkstyle trying to run on internal .git files

--- javatrix/Matrix.java
+++ javatrix/Matrix.java
@@ -1,5 +1,16 @@
 package javatrix;
 
-public class Matrix {
+/**
+ * A class that provides the functionality of a basic matrx.
+ * 
+ * @author Matt Stone
+ * @author Chad Halvorsen
+ * @author Mikeal Anderson
+ * 
+ * @version 2-7-17
+ * 
+ */
+public class Matrix
+{
 
 }
--- javatrix/tests/MatrixBasicTests.java
+++ javatrix/tests/MatrixBasicTests.java
@@ -1,31 +1,56 @@
 package javatrix.tests;
 
-import static org.junit.Assert.*;
+//import static org.junit.Assert.*;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-public class MatrixBasicTests {
+/**
+ * @author Matt Stone
+ * @author Chad Halvorsen
+ * @author Mikeal Anderson
+ * 
+ * @version 2-7-17
+ * 
+ */
+public class MatrixBasicTests
+{
 
-	@Before
-	public void setup(){
-		
-	}
-	
-	@After
-	public void teardown(){
-		
-	}
-	
-	@Test
-	public void exampletest1() {
-		System.out.println("You passed test1");
-	}
-	
-	@Test
-	public void exampletest2() {
-		System.out.println("You passed test2");
-	}
+    /**
+     * Basic set up before any test is run.
+     */
+    @Before
+    public void setup()
+    {
+
+    }
+
+    /**
+     * Basic teardown after a test is run.
+     */
+    @After
+    public void teardown()
+    {
+
+    }
+
+    /**
+     * Demo example test 1.
+     */
+    @Test
+    public void exampletest1()
+    {
+        System.out.println("You passed test1");
+    }
+
+    /**
+     * Demo example test2.
+     */
+    @Test
+    public void exampletest2()
+    {
+        System.out.println("You passed test2");
+    }
 
 }
