a17b6face2b88e8cca01885ec813d690d7acbb60
Matt Stone <mattstone22133@gmail.com>
2017-02-07 11:37:03 -0500

    updated the test file so we have some examples to work with

--- javatrix/tests/MatrixBasicTests.java
+++ javatrix/tests/MatrixBasicTests.java
@@ -2,13 +2,30 @@ package javatrix.tests;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class MatrixBasicTests {
 
+	@Before
+	public void setup(){
+		
+	}
+	
+	@After
+	public void teardown(){
+		
+	}
+	
 	@Test
-	public void test() {
-		System.out.println("You passed");
+	public void exampletest1() {
+		System.out.println("You passed test1");
+	}
+	
+	@Test
+	public void exampletest2() {
+		System.out.println("You passed test2");
 	}
 
 }
