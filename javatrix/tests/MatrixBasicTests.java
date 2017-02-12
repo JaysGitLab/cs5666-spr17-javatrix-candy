c63091122426f03428d9bec901711a24fed3bfdd
Matt Stone <mattstone22133@gmail.com>
2017-02-12 14:27:26 -0500

    added a test for the arrayTimes method

--- javatrix/tests/MatrixBasicTests.java
+++ javatrix/tests/MatrixBasicTests.java
@@ -2,6 +2,14 @@ package javatrix.tests;
 
 //import static org.junit.Assert.*;
 
+import static org.junit.Assert.assertTrue;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import javatrix.Matrix;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -42,6 +50,63 @@ public class MatrixBasicTests
     public void exampletest2()
     {
         // System.out.println("You passed test2");
+    }
+
+    /**
+     * Demo example test2.
+     */
+    @Test
+    public void testArrayTimes()
+    {
+        // create two matrices to multiply
+        Matrix one = new Matrix(4, 4, 10);
+        Matrix two = new Matrix(4, 4, 2);
+        String methodName = "arrayTimes";
+
+        // get method
+        try
+        {
+            // get a reflection to method (like a function pointer)
+            Method arrayTimesPointer = Matrix.class.getMethod("methodName",
+                    Matrix.class);
+            // set the accessibility to public
+            arrayTimesPointer.setAccessible(true);
+
+            // invoke method and check results
+            Matrix three = (Matrix) arrayTimesPointer.invoke(one, two);
+            Field internalMatrixField = Matrix.class.getField("matrix");
+            double[][] privMatrix = (double[][]) internalMatrixField.get(three);
+            for (int i = 0; i < privMatrix.length; ++i)
+            {
+                for (int j = 0; j < privMatrix[i].length; ++j)
+                {
+                    assertTrue("invalid value in patrix: " + privMatrix[i][j]
+                            + "should be" + 20.0 + ".",
+                            privMatrix[i][j] == 20.0);
+                }
+            }
+        }
+        catch (NoSuchMethodException e)
+        {
+            e.printStackTrace();
+            assertTrue("Cannot find method: " + methodName, false);
+        }
+        catch (SecurityException e)
+        {
+            e.printStackTrace();
+            assertTrue("security exception ", false);
+        }
+        catch (InvocationTargetException e)
+        {
+            e.printStackTrace();
+            assertTrue("failed to invoke the " + methodName + ".", false);
+        }
+        catch (Exception all)
+        {
+            all.printStackTrace();
+            assertTrue("Exception occured - stack trace printed", false);
+        }
+
     }
 
 }
